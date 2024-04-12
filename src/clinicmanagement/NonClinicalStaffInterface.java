94dab7e58f90f9f4c547d4474f8ca37d5a1eb4e7
sarkarrajorshi <153664352+sarkarrajorshi@users.noreply.github.com>
2024-04-12 17:20:54 -0400

    Delete src directory
    Refreshing the Files

--- src/clinicmanagement/NonClinicalStaffInterface.java
+++
@@ -1,8 +0,0 @@
-package clinicmanagement;
-
-/**
- * The NonClinicalStaffInterface extends StaffInterface and.
- * Ads methods specific to non-clinical staff.
- */
-interface NonClinicalStaffInterface extends StaffInterface {
-}
